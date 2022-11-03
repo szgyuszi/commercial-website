c2fa3d203865b4882f316ca3d31b5404685f509e
szgyuszi <szgybence@gmail.com>
2022-11-03 13:22:29 +0100

    fix: tailwind working properly

--- commercial-app/src/App.tsx
+++ commercial-app/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+
 function App() {
-  return (
-    <div>
-    Hello
-    </div>
-  );
+    return (
+        <div className="bg-white">
+            <h1 className="text-3xl font-bold text-gray-700">
+                Simple React Typescript Tailwind Sample
+            </h1>
+        </div>
+
+);
 }
 
 export default App;

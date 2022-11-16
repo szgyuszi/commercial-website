1a996960745ad22c9444dcd66249d8acc257ad4c
szgyuszi <szgybence@gmail.com>
2022-11-16 19:39:45 +0100

    feat: implement sidebar in main page

---
+++ commercial-app/src/common/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <main className="sticky top-20 bg-gray-200 w-1/6 max-[640px]:hidden h-screen flex justify-start items-start">
+      <div>Discover</div>
+    </main>
+  );
+};
+
+export default Sidebar;

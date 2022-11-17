76152f62717fe4664fafcc950af075a7d2052b50
szgyuszi <szgybence@gmail.com>
2022-11-17 13:12:55 +0100

    feat: implement states and useEffect for categories

--- commercial-app/src/common/Sidebar.tsx
+++ commercial-app/src/common/Sidebar.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { getCategories } from "../utils/fetch";
+
+interface category {
+  id: number;
+  name: string;
+}
 
 const Sidebar = () => {
+  const [categories, setCategories] = useState<category | null>(null);
+
+  useEffect(() => {
+    const getCategoriesData = async () => {
+      const data = await getCategories();
+      setCategories(data);
+    };
+
+    getCategoriesData();
+  }, []);
+
   return (
     <main className="sticky top-20 bg-gray-200 w-1/6 max-[640px]:hidden h-screen flex justify-start items-start">
       <div>Discover</div>

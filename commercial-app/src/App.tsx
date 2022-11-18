d253ce2f104f2eb2cfca135bcb06481419855215
szgyuszi <szgybence@gmail.com>
2022-11-18 14:57:23 +0100

    feat: wrap Routes around user context

--- commercial-app/src/App.tsx
+++ commercial-app/src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import { useState } from "react";
 import RoutesManager from "./setup/routes-manager";
-
-
+import { UserContextInterface } from "./utils/modal";
+import { UserContext } from "./setup/app-context-manager/application-context-manager";
 
 function App() {
-    return (
-        <RoutesManager />
-    );
+  const [user, setUser] = useState<UserContextInterface | null>(null);
+
+  return (
+    <UserContext.Provider value={user}>
+      <RoutesManager />
+    </UserContext.Provider>
+  );
 }
 
 export default App;

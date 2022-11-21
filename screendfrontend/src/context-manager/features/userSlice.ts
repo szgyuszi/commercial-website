aa21e83b5d5a1338d2da2d6f04a563d00139c126
szgyuszi <szgybence@gmail.com>
2022-11-21 11:21:19 +0100

    feat: add userSlice and reducers

---
+++ screendfrontend/src/context-manager/features/userSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { UserContextInterface } from "../../utils/modal";
+import type { RootState } from "../store";
+
+const initialState: UserContextInterface = {
+  id: null,
+  name: null,
+  userImg: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    logUserIn: (state, action) => {
+      state.id = action.payload.id;
+      state.name = action.payload.name;
+      state.userImg = action.payload.userImg;
+    },
+    logUserOut: (state) => {
+      state.id = null;
+      state.name = null;
+      state.userImg = null;
+    },
+  },
+});
+
+export const { logUserIn, logUserOut } = userSlice.actions;
+
+export const user = (state: RootState) => state.user;
+
+export default userSlice.reducer;

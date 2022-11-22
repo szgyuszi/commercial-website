da7d9d0a6bad7d80bd3891c8f78050e4a54d0020
szgyuszi <szgybence@gmail.com>
2022-11-22 13:16:01 +0100

    feat: add profile component to route

--- screendfrontend/src/setup/routes-manager/index.tsx
+++ screendfrontend/src/setup/routes-manager/index.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "../../common/Header";
 import LandingPage from "../../pages/landing/LandingPage";
 import MainLayout from "../../pages/main/components/MainLayout";
+import ProfilePage from "../../pages/profile/ProfilePage";
 import Login from "../../pages/sign-in/Login";
 import Register from "../../pages/sign-up/Register";
 
@@ -14,7 +15,7 @@ function RoutesManager() {
         <Route path="*" element={<h1>No page</h1>} />
       </Route>
       <Route path="/post/:id" element={<h1>PostById</h1>} />
-      <Route path="/user/:id" element={<h1>UserById</h1>} />
+      <Route path="/user/:id" element={<ProfilePage />} />
       <Route path="/sign-in" element={<Login />} />
       <Route path="/sign-up" element={<Register />} />
       <Route path="*" element={<h1>No page</h1>} />

304db98db76ec3b9d744075dd4c4cb96c2874df2
szgyuszi <szgybence@gmail.com>
2022-11-22 20:17:38 +0100

    feat: implement query for getting posts by user id

--- screendbackend/src/data/queries.js
+++ screendbackend/src/data/queries.js
@@ -1,7 +1,7 @@
 const {createUser, deleteUser, getUserById, loginUser} = require('./user-queries/userQueries')
-const {getPosts, createPost, getPostById, deletePost}  = require('./posts-queries/postsQueries');
+const {getPosts, createPost, getPostById, deletePost, getPostsByUserId}  = require('./posts-queries/postsQueries');
 const {getCategories}  = require('./category-queries/categoryQueries');
 
 module.exports = {
-    getPosts, getUserById, createUser, deleteUser, createPost, getPostById, deletePost, getCategories, loginUser
+    getPosts, getUserById, createUser, deleteUser, createPost, getPostById, deletePost, getCategories, loginUser, getPostsByUserId
 }

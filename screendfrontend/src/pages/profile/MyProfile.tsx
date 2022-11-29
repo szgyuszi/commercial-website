1d96e66e5d17c022355d791351d013fdf3b9e091
szgyuszi <szgybence@gmail.com>
2022-11-29 11:10:17 +0100

    refactor: re-organize profile pages

--- screendfrontend/src/pages/profile/MyProfile.tsx
+++ screendfrontend/src/pages/profile/MyProfile.tsx
@@ -1,37 +1,33 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import LoadingSpinner from "../../common/LoadingSpinner";
 import MasonryLayout from "../../common/MasonryLayout";
 import { userState } from "../../context-manager/features/userSlice";
-
 import { useAppSelector } from "../../context-manager/hooks";
 import { getPostsByUserId } from "../../utils/fetch";
 import { Post, UserContextInterface } from "../../utils/modal";
 
-const ProfilePage = () => {
-  const user: UserContextInterface = useAppSelector(userState);
+const MyProfile = () => {
+  const currentUser: UserContextInterface = useAppSelector(userState);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
   const [posts, setPosts] = useState<Post[]>([]);
 
-  const id = useParams().id;
-  console.log(id);
+  const getPostsById = async () => {
+    return await getPostsByUserId(currentUser.id!);
+  };
 
   useEffect(() => {
-    // fetch posts
-    const getPosts = async () => {
-      setLoading(true);
-      const res = await getPostsByUserId(user.id ? user.id : id ? id : 0);
-      console.log(res);
+    setLoading(true);
+    getPostsById().then((res) => {
       if (res.error) {
-        setLoading(false);
         setError(true);
-      } else {
         setLoading(false);
-        setPosts(res);
+        return;
       }
-    };
-    getPosts();
+      setPosts(res);
+      setError(false);
+      setLoading(false);
+    });
   }, []);
 
   return (
@@ -40,16 +36,18 @@ const ProfilePage = () => {
         <div className="relative top-24 border-4 border-white border-solid rounded-full p-2 bg-white">
           <img
             className="rounded-full "
-            src={user.userImg ? user.userImg : ""}
-            alt={user.name ? user.name : ""}
+            src={currentUser.userImg!}
+            alt={currentUser.name!}
           />
         </div>
       </section>
       <section className="mt-28 w-full flex justify-center items-center">
-        <p className="text-3xl">{user.name}</p>
+        <p className="text-3xl">{currentUser.name}</p>
       </section>
       <section className="w-full h-auto flex justify-center items-center mt-6 flex-col">
-        {loading ? (
+        {error ? (
+          <p className="text-5xl mb-3">Uups, something went wrong.. :\</p>
+        ) : loading ? (
           <LoadingSpinner />
         ) : (
           <>
@@ -64,4 +62,4 @@ const ProfilePage = () => {
   );
 };
 
-export default ProfilePage;
+export default MyProfile;

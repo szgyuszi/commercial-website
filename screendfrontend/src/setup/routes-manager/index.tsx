8408d6f6af67756b3f5192c1bf2c6685867e8ba5
szgyuszi <szgybence@gmail.com>
2022-11-29 11:10:42 +0100

    refactor: add the specific components to routes

--- screendfrontend/src/setup/routes-manager/index.tsx
+++ screendfrontend/src/setup/routes-manager/index.tsx
@@ -2,7 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Header from "../../common/Header";
 import LandingPage from "../../pages/landing/LandingPage";
 import MainLayout from "../../pages/main/components/MainLayout";
-import ProfilePage from "../../pages/profile/ProfilePage";
+import MyProfile from "../../pages/profile/MyProfile";
+import Profile from "../../pages/profile/Profile";
 import Login from "../../pages/sign-in/Login";
 import Register from "../../pages/sign-up/Register";
 
@@ -13,9 +14,10 @@ function RoutesManager() {
       <Route path="/" element={<Header />}>
         <Route index element={<MainLayout />} />
         <Route path="*" element={<h1>No page</h1>} />
+        <Route path="/user/:id" element={<Profile />} />
       </Route>
+      <Route path="/my-profile" element={<MyProfile />} />
       <Route path="/post/:id" element={<h1>PostById</h1>} />
-      <Route path="/user/:id" element={<ProfilePage />} />
       <Route path="/sign-in" element={<Login />} />
       <Route path="/sign-up" element={<Register />} />
       <Route path="*" element={<h1>No page</h1>} />

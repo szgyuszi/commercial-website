4330e898a3e73a1d08e04ea3efaf5cac6da67de1
szgyuszi <szgybence@gmail.com>
2022-12-06 12:34:26 +0100

    feat: add more users

--- screendbackend/src/data/database.sql
+++ screendbackend/src/data/database.sql
@@ -38,34 +38,40 @@ INSERT INTO categories VALUES (3, 'Ninja');
 
 INSERT INTO "users"
 (id, name, email, password, img) VALUES (1, 'Gyuszi', 'szgybence@gmail.com', '1234', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1zcJ0i7HDB64UCj8ROPXIwW7FeDViAzmAHoq3Ix8FWIj0i_M2CrHNqnYgPKSsjfI7MAU&usqp=CAU');
-SELECT pg_catalog.setval('users_id_seq', 2, true);
+INSERT INTO "users"
+(id, name, email, password, img) VALUES (2, 'Hanna', 'hanna@gmail.com', '1234', 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg');
+INSERT INTO "users"
+(id, name, email, password, img) VALUES (3, 'Andro', 'andro@gmail.com', '1234', 'https://hips.hearstapps.com/hmg-prod/images/street-portrait-of-a-young-man-using-mobile-phone-royalty-free-image-1018047498-1564431457.jpg?crop=0.668xw:1.00xh;0.226xw,0&resize=640:*');
+INSERT INTO "users"
+(id, name, email, password, img) VALUES (4, 'Kristóf', 'kristof553@gmail.com', '1234', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJZFNuDRtYMhHexSNxefiKhhDHsOoFHx9DkzmGKtOENhu8J-Iq2yj8eb3HYTtDoeD0ODU&usqp=CAU');
+SELECT pg_catalog.setval('users_id_seq', 5, true);
 
 INSERT INTO posts
 (id, title, img, date, likes, userId, categoryId) VALUES (1, 'Junky Times', 'https://i.pinimg.com/236x/5c/ea/85/5cea85f1551b21bcc52eb667e40eeedd.jpg', now(), 4, 1, 2);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (2, 'Sekiro', 'https://i.pinimg.com/236x/01/34/1a/01341a0232b3883c61a8c3b314f3d61c.jpg', now(), 14, 1, 3);
+(id, title, img, date, likes, userId, categoryId) VALUES (2, 'Sekiro', 'https://i.pinimg.com/236x/01/34/1a/01341a0232b3883c61a8c3b314f3d61c.jpg', now(), 14, 2, 3);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (3, 'xdddd', 'https://i.pinimg.com/236x/80/be/ce/80bece137a38be5c9fc62d7b4db4dcf4.jpg', now(), 6, 1, 1);
+(id, title, img, date, likes, userId, categoryId) VALUES (3, 'xdddd', 'https://i.pinimg.com/236x/80/be/ce/80bece137a38be5c9fc62d7b4db4dcf4.jpg', now(), 6, 2, 1);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (4, 'What the dog doin?', 'https://i.pinimg.com/236x/b6/d4/31/b6d43146b0c987a5a40c14536344bb7d.jpg', now(), 23, 1, 1);
+(id, title, img, date, likes, userId, categoryId) VALUES (4, 'What the dog doin?', 'https://i.pinimg.com/236x/b6/d4/31/b6d43146b0c987a5a40c14536344bb7d.jpg', now(), 23, 3, 1);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (5, 'O.O', 'https://i.pinimg.com/236x/b5/ed/f1/b5edf1b0ec30c194f7516946960bf4b8.jpg', now(), 1, 1, 2);
+(id, title, img, date, likes, userId, categoryId) VALUES (5, 'O.O', 'https://i.pinimg.com/236x/b5/ed/f1/b5edf1b0ec30c194f7516946960bf4b8.jpg', now(), 1, 4, 2);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (6, 'uwuU', 'https://i.pinimg.com/236x/68/9f/5b/689f5bcdf3c6886b98d02d32088466b8.jpg', now(), 21, 1, 2);
+(id, title, img, date, likes, userId, categoryId) VALUES (6, 'uwuU', 'https://i.pinimg.com/236x/68/9f/5b/689f5bcdf3c6886b98d02d32088466b8.jpg', now(), 21, 3, 2);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (7, 'Fun Times', 'https://i.pinimg.com/236x/be/6b/61/be6b612ba63f00ed8de4a9ff5c6d38d1.jpg', now(), 44, 1, 1);
+(id, title, img, date, likes, userId, categoryId) VALUES (7, 'Fun Times', 'https://i.pinimg.com/236x/be/6b/61/be6b612ba63f00ed8de4a9ff5c6d38d1.jpg', now(), 44, 3, 1);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (8, 'Ninjazz', 'https://i.pinimg.com/236x/a9/20/28/a9202818e5eac8d774c420a979ab7354.jpg', now(), 7, 1, 3);
+(id, title, img, date, likes, userId, categoryId) VALUES (8, 'Ninjazz', 'https://i.pinimg.com/236x/a9/20/28/a9202818e5eac8d774c420a979ab7354.jpg', now(), 7, 2, 3);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (9, 'Funneh', 'https://i.pinimg.com/236x/24/f7/88/24f7881cd48df20ab782864b7736c15b.jpg', now(), 8, 1, 1);
+(id, title, img, date, likes, userId, categoryId) VALUES (9, 'Funneh', 'https://i.pinimg.com/236x/24/f7/88/24f7881cd48df20ab782864b7736c15b.jpg', now(), 8, 4, 1);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (10, 'Sekiro ultimate', 'https://i.pinimg.com/236x/6a/68/95/6a68957dd78e244b3c31b99071b9e662.jpg', now(), 9, 1, 3);
+(id, title, img, date, likes, userId, categoryId) VALUES (10, 'Sekiro ultimate', 'https://i.pinimg.com/236x/6a/68/95/6a68957dd78e244b3c31b99071b9e662.jpg', now(), 9, 4, 3);
 INSERT INTO posts
 (id, title, img, date, likes, userId, categoryId) VALUES (11, 'Minimalist wierdness', 'https://i.pinimg.com/236x/e6/f6/b8/e6f6b8e5f8ad4ed8cba8fa7bf174f011.jpg', now(), 42, 1, 2);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (12, 'ShtPost', 'https://i.pinimg.com/236x/69/ad/cc/69adcc2f223c0a354f8de46c52fb8c3d.jpg', now(), 23, 1, 1);
+(id, title, img, date, likes, userId, categoryId) VALUES (12, 'ShtPost', 'https://i.pinimg.com/236x/69/ad/cc/69adcc2f223c0a354f8de46c52fb8c3d.jpg', now(), 23, 4, 1);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (13, 'Nani', 'https://i.pinimg.com/236x/73/61/2d/73612d2d9aecab0c158b80855eb0c790.jpg', now(), 54, 1, 3);
+(id, title, img, date, likes, userId, categoryId) VALUES (13, 'Nani', 'https://i.pinimg.com/236x/73/61/2d/73612d2d9aecab0c158b80855eb0c790.jpg', now(), 54, 2, 3);
 INSERT INTO posts
-(id, title, img, date, likes, userId, categoryId) VALUES (14, 'Omaiva Mo', 'https://i.pinimg.com/236x/7b/73/21/7b732167760cc245ee45ed8ca6362ce9.jpg', now(), 77, 1, 3);
+(id, title, img, date, likes, userId, categoryId) VALUES (14, 'Omaiva Mo', 'https://i.pinimg.com/236x/7b/73/21/7b732167760cc245ee45ed8ca6362ce9.jpg', now(), 77, 3, 3);
 SELECT pg_catalog.setval('posts_id_seq', 15, true);
